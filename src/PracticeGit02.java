5374a4873b411c8f9e6430dd4f9e18f720cd4dcd
Prashant Kumar <softwaretesterKumar@gmail.com>
2024-03-13 14:31:53 +0000

    Find the value

---
+++ src/PracticeGit02.java
@@ -0,0 +1,17 @@
+public class PracticeGit02
+
+{
+    int a = 21;
+    int b = 88;
+    int c = b/a;
+
+    public static void main(String[] args) {
+
+        System.out.println("Find the value of c");
+
+    }
+
+
+
+
+}
